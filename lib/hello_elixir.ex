9cd6a056dc57685c093b72e8ecf0e4048458afba
Viktor Kanaev <vikkanaev@gmail.com>
2021-11-19 00:09:46 +0300

    add code and test

---
+++ lib/hello_elixir.ex
@@ -0,0 +1,18 @@
+defmodule HelloElixir do
+  @moduledoc """
+  Documentation for `HelloElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloElixir.hello()
+      'Hello world'
+
+  """
+  def hello do
+    'Hello world'
+  end
+end
