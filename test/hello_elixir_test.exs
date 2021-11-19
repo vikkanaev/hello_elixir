5e459eaca40b34c8c60dc9fca3b8254baa1970b7
Viktor Kanaev <vikkanaev@gmail.com>
2021-11-19 12:11:14 +0300

    Add ex_doc

--- test/hello_elixir_test.exs
+++ test/hello_elixir_test.exs
@@ -2,6 +2,7 @@ defmodule HelloElixirTest do
   use ExUnit.Case
   doctest HelloElixir
 
+  # NOTE: Best practice do not repeat tests from docset in ExUnit. This test just for example
   test "greets the world" do
     assert HelloElixir.hello() == 'Hello world'
   end
